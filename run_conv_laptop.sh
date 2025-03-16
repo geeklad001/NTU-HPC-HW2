8758ec261b2a25bd4c8da577e95aa6ca440aea8c
geeklad13922135 <geeklad13922135@gmail.com>
2025-03-16 21:25:19 +0800

    some change on the test script

--- run_conv_laptop.sh
+++ run_conv_laptop.sh
@@ -4,7 +4,7 @@
 matrix_sizes=(256 512 1024 4096)  # Matrix sizes
 kernel_sizes=(3 5 7 9)            # Kernel sizes
 num_threads=(1 2 4)       # Number of threads
-num_runs=100                      # Number of times to repeat each test
+num_runs=30                      # Number of times to repeat each test
 
 # Output file for collecting data
 output_file="execution_times_c2_dy_blk_reduc(local).csv"
@@ -19,7 +19,7 @@ gcc conv-openmp.c -o conv-openmp -fopenmp
 # Loop through all combinations of matrix size and kernel size
 for matrix_size in "${matrix_sizes[@]}"; do
     for kernel_size in "${kernel_sizes[@]}"; do
-        # Run the non-OpenMP version 100 times
+        # Run the non-OpenMP version for specific number of times
         for run_id in $(seq 1 $num_runs); do
             echo "Running non-OpenMP: Matrix ${matrix_size}x${matrix_size}, Kernel ${kernel_size}, Run ${run_id}..."
             
@@ -30,7 +30,7 @@ for matrix_size in "${matrix_sizes[@]}"; do
             echo "${matrix_size}x${matrix_size},${kernel_size},1,${run_id},${exec_time}" >> $output_file
         done
 
-        # Run the OpenMP version for different thread counts 100 times each
+        # Run the OpenMP version for different thread counts for specific number of times each
         for threads in "${num_threads[@]}"; do
             for run_id in $(seq 1 $num_runs); do
                 echo "Running OpenMP: Matrix ${matrix_size}x${matrix_size}, Kernel ${kernel_size}, Threads ${threads}, Run ${run_id}..."
