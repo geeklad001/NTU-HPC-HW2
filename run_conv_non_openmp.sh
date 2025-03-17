89134d25ffeabddb98321d3274f050a1f26282bb
geeklad13922135 <geeklad13922135@gmail.com>
2025-03-17 21:28:19 +0800

    fix more scripts

--- run_conv_non_openmp.sh
+++ run_conv_non_openmp.sh
@@ -3,7 +3,7 @@
 # Define the variations for matrix size, kernel size, and number of threads
 matrix_sizes=(256 512 1024 4096)  # Matrix sizes
 kernel_sizes=(3 5 7 9)            # Kernel sizes
-num_runs=30                       # Number of times to repeat each test
+num_runs=20                       # Number of times to repeat each test
 
 # Output file for collecting data
 output_file="execution_times_non_openmp.csv"
@@ -23,11 +23,11 @@ for matrix_size in "${matrix_sizes[@]}"; do
             
             # Collect execution time and correctness
             result=$(./conv "mat-${matrix_size}.txt" "ker-${kernel_size}.txt" "ans-${matrix_size}-${kernel_size}.txt" | grep "Time taken")
-            exec_time=$(echo $result | awk '{print $4}')
-            correctness=$(echo $result | awk '{print $1}')
+            exec_time=$(echo "$result" | grep "Time taken" | awk '{print $4}')
+            correctness=$(echo "$result" | grep "Time taken" | awk '{print $1}')
 
             # Append the result to the CSV file
-            echo "${matrix_size}x${matrix_size},${kernel_size},1,${run_id},${exec_time},${correctness}" >> $output_file
+            echo "${matrix_size}x${matrix_size},${kernel_size},0,${run_id},${exec_time},${correctness}" >> $output_file
         done
     done
 done
