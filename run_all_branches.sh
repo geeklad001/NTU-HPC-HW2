fe2ed10a1f69cddfb1dc38b81924b099bec2af83
geeklad13922135 <geeklad13922135@gmail.com>
2025-04-01 05:48:29 +0800

    add run all code

--- run_all_branches.sh
+++ run_all_branches.sh
@@ -12,6 +12,7 @@ bash $non_openmp_script_path
 for branch in "${branches[@]}"; do
     echo "Checking out branch: $branch"
     git checkout $branch
+    git pull
 
     # Run the script
     echo "Running script on branch: $branch"
@@ -20,9 +21,10 @@ for branch in "${branches[@]}"; do
     # Add and commit the results to the repository
     git add *.csv
     git commit -m "Save output data for branch ${branch}"
+    git push
 done
 
 # Push all changes to the remote repository
-git push origin --all
+# git push origin --all
 
 echo "All branches processed and results pushed to remote repository."
