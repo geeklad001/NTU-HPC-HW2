95d1684fc6bf600faa3ad3d2c2fa1c38e7868240
geeklad13922135 <geeklad13922135@gmail.com>
2025-04-01 10:10:51 +0800

    update run_remaining

--- run_remaining.sh
+++ run_remaining.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # List of branches to run the script on
-branches=("dy" "guided" "static")
+branches=("dy" "guided")
 
 # Path to the run_conv_server.sh script
 non_openmp_script_path="./run_conv_non_openmp.sh"
 openmp_script_path="./run_conv_openmp.sh"
 
 # Loop through each branch
-bash $non_openmp_script_path
 for branch in "${branches[@]}"; do
     echo "Checking out branch: $branch"
     git checkout $branch
+    git restore *.sh
+    git pull
 
     # Run the script
     echo "Running script on branch: $branch"
@@ -20,9 +21,10 @@ for branch in "${branches[@]}"; do
     # Add and commit the results to the repository
     git add *.csv
     git commit -m "Save output data for branch ${branch}"
+    git push
 done
 
 # Push all changes to the remote repository
-git push origin --all
+# git push origin --all
 
 echo "All branches processed and results pushed to remote repository."
